58112738c6b17ee937fbfdc45b8ce246115b9629
Theo Lima <theo.suporte@hotmail.com>
2021-08-11 16:43:52 -0300

    Resolve exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exercicio-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exercicio-1.js
@@ -2,10 +2,10 @@ let n = 5;
 let symbol = '*';
 let inputLine = '';
 
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+for (let index = 0; index < n; index += 1) {
   inputLine = inputLine + symbol;
 };
 
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+for (let index = 0; index < n; index += 1) {
   console.log(inputLine);
 };
